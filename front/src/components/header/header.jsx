cb76dfbc38c0c27603bb4c7e917faa0cd0c5c942
Sheethal Joshi <sh33thal24@gmail.com>
2023-10-16 00:10:15 +0530

    added css file

--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './header.css';
 
 const Header = () => {
-    return (
-        <div className="mt-5 ml-5 2.5rem p-0.5rem flex items-center justify-between">
-            <div className="flex items-center justify-between">
-            <Link to ="/"><img className="w-32 cursor-pointer" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"/></Link>
-            </div>
-            <div className="flex mr-6 text-xl space-x-20 font-semibold">
-            <Link to ="/movies/popular" className="text-white hover:text-red-500 cursor-pointer"><span>POPULAR</span></Link>
-            <Link to ="/movies/toprated"className="text-white hover:text-red-500 cursor-pointer" ><span>TOP MOVIES</span></Link>
-            <Link to ="/movies/upcoming" className="text-white hover:text-red-500 cursor-pointer"><span>UPCOMING</span></Link>
-            </div>
-            
-        </div>
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="relative p-5 flex items-center justify-between md:px-10 z-10">
+            <div className="flex items-center">
+                <Link to="/">
+                    <img className="w-32 cursor-pointer" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="IMDB Logo" />
+                </Link>
+            </div>
+
+            <div className="hidden md:flex text-xl space-x-10 font-semibold">
+                <Link to="/movies/popular" className="text-white hover:text-red-500 cursor-pointer">POPULAR</Link>
+                <Link to="/movies/toprated" className="text-white hover:text-red-500 cursor-pointer">TOP MOVIES</Link>
+                <Link to="/movies/upcoming" className="text-white hover:text-red-500 cursor-pointer">UPCOMING</Link>
+            </div>
+
+            {/* Hamburger Menu */}
+            <div className="md:hidden relative z-20">
+                <button onClick={toggleMenu} className="text-xl text-white cursor-pointer">
+                    ☰
+                </button>
+                {isOpen && (
+                    <div className="absolute top-16 right-0 mt-2 bg-black rounded-lg shadow-lg p-5 text-white">
+                        <Link to="/movies/popular" className="block py-2 text-white hover:text-red-500 cursor-pointer">POPULAR</Link>
+                        <Link to="/movies/toprated" className="block py-2 text-white hover:text-red-500 cursor-pointer">TOP MOVIES</Link>
+                        <Link to="/movies/upcoming" className="block py-2 text-white hover:text-red-500 cursor-pointer">UPCOMING</Link>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
+
 export default Header;
